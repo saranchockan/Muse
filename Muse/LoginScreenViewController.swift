9e4296bb5ff453a0fdfac333620a1c481b533833
Elizabeth Snider <elizabethsnider2011@gmail.com>
2023-02-28 13:35:12 -0600

    Added splash screen and base for login screen

---
+++ Muse/LoginScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginScreenViewController.swift
+//  Muse
+//
+//  Created by Elizabeth Snider on 2/28/23.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Muse/SplashScreen.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Muse
+//
+//  Created by Saahithi Joopelli on 2/28/23.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginScreenIdentifier") as! LoginScreenViewController
+            self.present(nextVC, animated: true)
+        }
+    }
+
+}
+
