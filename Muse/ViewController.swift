bf027c7df9424449e5b7472660ef781889855549
Saahithi Joopelli <sjoopelli@gmail.com>
2023-02-28 11:36:07 -0600

    Initial commit

---
+++ Muse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Muse
+//
+//  Created by Saahithi Joopelli on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
