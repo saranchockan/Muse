1b06810af1c1722cbf60c6174e87f7fdaeae83b7
Saran Chockan <saran.arun@utexas.edu>
2023-03-03 15:19:18 -0600

    added spotify auth

--- Muse/ConnectSpotifyViewController.swift
+++ Muse/ConnectSpotifyViewController.swift
@@ -6,24 +6,44 @@
 //
 
 import UIKit
+import SpotifyWebAPI
 
 class ConnectSpotifyViewController: UIViewController {
-
+    let SPOTIFY_CLIENT_ID = "658a9575d9cf4542a28d885c7000b725"
+    let SPOTIFY_CLIENT_SECRET = "46219117f32042b9a217cde1d3155a09"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Create an instance of SpotifyAPI
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func connectToSpotify(_ sender: Any) {
+        let spotifyAPI = SpotifyAPI(
+            authorizationManager: AuthorizationCodeFlowManager(
+                clientId: self.SPOTIFY_CLIENT_ID, clientSecret: self.SPOTIFY_CLIENT_SECRET
+            )
+        )
+        let url = spotifyAPI.authorizationManager.makeAuthorizationURL(
+            redirectURI: URL(string: "muse://login-callback")!,
+                    showDialog: true,
+                    state: String.randomURLSafe(length: 128),
+                    scopes: [
+                        .userReadPlaybackState,
+                        .userModifyPlaybackState,
+                        .playlistModifyPrivate,
+                        .playlistModifyPublic,
+                        .userLibraryRead,
+                        .userLibraryModify,
+                        .userReadRecentlyPlayed
+                    ]
+                )!
+                
+                // You can open the URL however you like. For example, you could open
+                // it in a web view instead of the browser.
+                // See https://developer.apple.com/documentation/webkit/wkwebview
+                UIApplication.shared.open(url)
     }
-    */
-
+    
+    
+    
 }
