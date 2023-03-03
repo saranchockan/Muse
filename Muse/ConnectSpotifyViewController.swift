6333ab7e04baa295f18cc189c261c75e6c61f923
Saran Chockan <saran.arun@utexas.edu>
2023-03-03 15:56:49 -0600

    cleaned up spotify auth

--- Muse/ConnectSpotifyViewController.swift
+++ Muse/ConnectSpotifyViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 import SpotifyWebAPI
 
 class ConnectSpotifyViewController: UIViewController {
-    let SPOTIFY_CLIENT_ID = "658a9575d9cf4542a28d885c7000b725"
-    let SPOTIFY_CLIENT_SECRET = "46219117f32042b9a217cde1d3155a09"
+    let SPOTIFY_CLIENT_ID_KEY: String = "SPOTIFY_CLIENT_ID"
+    let SPOTIFY_CLIENT_SECRET_KEY:String = "SPOTIFY_CLIENT_SECRET"
+    let SPOTIFY_AUTH_REDIRECT_URI:String = "muse://login-callback"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,30 +21,27 @@ class ConnectSpotifyViewController: UIViewController {
     @IBAction func connectToSpotify(_ sender: Any) {
         let spotifyAPI = SpotifyAPI(
             authorizationManager: AuthorizationCodeFlowManager(
-                clientId: self.SPOTIFY_CLIENT_ID, clientSecret: self.SPOTIFY_CLIENT_SECRET
+                clientId: ProcessInfo.processInfo.environment[self.SPOTIFY_CLIENT_ID_KEY]!, clientSecret: ProcessInfo.processInfo.environment[self.SPOTIFY_CLIENT_SECRET_KEY]!
             )
         )
-        let url = spotifyAPI.authorizationManager.makeAuthorizationURL(
-            redirectURI: URL(string: "muse://login-callback")!,
-                    showDialog: true,
-                    state: String.randomURLSafe(length: 128),
-                    scopes: [
-                        .userReadPlaybackState,
-                        .userModifyPlaybackState,
-                        .playlistModifyPrivate,
-                        .playlistModifyPublic,
-                        .userLibraryRead,
-                        .userLibraryModify,
-                        .userReadRecentlyPlayed
-                    ]
-                )!
-                
-                // You can open the URL however you like. For example, you could open
-                // it in a web view instead of the browser.
-                // See https://developer.apple.com/documentation/webkit/wkwebview
-                UIApplication.shared.open(url)
+        let spotifyAuthorizationURL = spotifyAPI.authorizationManager.makeAuthorizationURL(
+            redirectURI: URL(string: self.SPOTIFY_AUTH_REDIRECT_URI)!,
+                showDialog: true,
+                state: String.randomURLSafe(length: 128),
+                scopes: [
+                    .userReadPlaybackState,
+                    .userModifyPlaybackState,
+                    .playlistModifyPrivate,
+                    .playlistModifyPublic,
+                    .userLibraryRead,
+                    .userLibraryModify,
+                    .userReadRecentlyPlayed,
+                    .userTopRead,
+                ]
+        )!
+        // You can open the URL however you like. For example, you could open
+        // it in a web view instead of the browser.
+        // See https://developer.apple.com/documentation/webkit/wkwebview
+        UIApplication.shared.open(spotifyAuthorizationURL)
     }
-    
-    
-    
 }
