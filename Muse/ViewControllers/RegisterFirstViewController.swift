08260a141064dd61a6501e746a6ea35c2adb4b46
Elizabeth Snider <elizabethsnider2011@gmail.com>
2023-03-03 19:19:52 -0600

    Finished register and welcome screen UIs connected with segues, added subdirectory for viewcontroller files

---
+++ Muse/ViewControllers/RegisterFirstViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RegisterFirstViewController.swift
+//  Muse
+//
+//  Created by Richa Gadre on 3/1/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class RegisterFirstViewController: UIViewController {
+
+    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        email.layer.cornerRadius = 30
+        let emailPaddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 31))
+        email.leftView = emailPaddingView
+        email.leftViewMode = .always
+        
+        password.layer.cornerRadius = 30
+        let passPaddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 31))
+        password.leftView = passPaddingView
+        password.leftViewMode = .always
+        
+        confirmPassword.layer.cornerRadius = 30
+        let confirmPaddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 31))
+        confirmPassword.leftView = confirmPaddingView
+        confirmPassword.leftViewMode = .always
+        
+        continueButton.layer.cornerRadius = 30
+
+    }
+    
+    @IBAction func continueToWelcomeScreen(_ sender: Any) {
+        userEmail = email.text!
+        userPassword = password.text!
+    }
+
+}
---
+++ Muse/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  WelcomeViewController.swift
+//  Muse
+//
+//  Created by Elizabeth Snider on 3/2/23.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        firstName.layer.cornerRadius = 30
+        let firstPaddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 31))
+        firstName.leftView = firstPaddingView
+        firstName.leftViewMode = .always
+        
+        lastName.layer.cornerRadius = 30
+        let lastPaddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 31))
+        lastName.leftView = lastPaddingView
+        lastName.leftViewMode = .always
+        
+        phoneNumber.layer.cornerRadius = 30
+        let phonePaddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 31))
+        phoneNumber.leftView = phonePaddingView
+        phoneNumber.leftViewMode = .always
+        
+        location.layer.cornerRadius = 30
+        let locationPaddingView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: 31))
+        location.leftView = locationPaddingView
+        location.leftViewMode = .always
+        
+        continueButton.layer.cornerRadius = 30
+    }
+    
+    
+    @IBAction func continueToConnectSpotify(_ sender: Any) {
+        userFirstName = firstName.text!
+        userLastName = lastName.text!
+        userPhoneNumber = phoneNumber.text!
+        userLocation = location.text!
+    }
+
+}
