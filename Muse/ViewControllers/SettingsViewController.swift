8523965afa3251221a3569800cd7f2ec35b2c0c7
Elizabeth Snider <elizabethsnider2011@gmail.com>
2023-03-07 18:17:15 -0600

    finish front end for alpha

--- Muse/ViewControllers/SettingsViewController.swift
+++ Muse/ViewControllers/SettingsViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var fullName: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +20,12 @@ class SettingsViewController: UIViewController {
         profilePicture.layer.borderColor = CGColor(red: 150/255, green: 150/255, blue: 219/255, alpha: 1)
     }
 
-
+    @IBAction func logout(_ sender: Any) {
+    }
+    
+    @IBAction func deleteAccount(_ sender: Any) {
+    }
+    
+    @IBAction func editProfilePicture(_ sender: Any) {
+    }
 }
