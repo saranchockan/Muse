6ce0a1a1715d8266e64890a64f9b1263e9861b04
Saahithi <sjoopelli@gmail.com>
2023-04-04 10:10:46 -0500

    created image picker

--- Muse/ViewControllers/SettingsViewController.swift
+++ Muse/ViewControllers/SettingsViewController.swift
@@ -8,17 +8,27 @@
 import UIKit
 import FirebaseAuth
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var location: UITextField!
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var fullName: UITextField!
     
+    let imagePicker: UIImagePickerController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         profilePicture.layer.borderColor = CGColor(red: 150/255, green: 150/255, blue: 219/255, alpha: 1)
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.mediaTypes = ["public.image"]
+        pickerController.sourceType = .photoLibrary
+        
+        print("media types: \(UIImagePickerController.availableMediaTypes(for: .photoLibrary))")
     }
 
     @IBAction func logout(_ sender: Any) {
