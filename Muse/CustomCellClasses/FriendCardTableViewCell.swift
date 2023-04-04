f655499d771a11f5cb8d6e35a5d4229c38a20e9a
Elizabeth Snider <elizabethsnider2011@gmail.com>
2023-04-04 15:52:20 -0500

    created friend card and added to my friends

---
+++ Muse/CustomCellClasses/FriendCardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FriendCardTableViewCell.swift
+//  Muse
+//
+//  Created by Elizabeth Snider on 4/3/23.
+//
+
+import UIKit
+
+class FriendCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
