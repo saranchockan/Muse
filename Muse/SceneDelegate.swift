ce08dd508d66d969c00ca79f32edfdd9330a2b2d
Saran Chockan <saran.arun@utexas.edu>
2023-04-05 15:32:20 -0500

    integrated spotify auth and data processing

--- Muse/SceneDelegate.swift
+++ Muse/SceneDelegate.swift
@@ -45,6 +45,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            connectSpotifyViewControllerInstance?.handleURL(url)
+        }
+    }
 
 }
 
--- Muse/ViewControllers/HomeViewController.swift
+++ Muse/ViewControllers/HomeViewController.swift
@@ -9,17 +9,25 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import FirebaseFirestoreSwift
-
+import SpotifyWebAPI
+import Combine
 
 var sharedArtists:[String:SharedArtist] = [:]
 var sharedSongs:[String: SharedSong] = [:]
 
+protocol SpotifyProtocol {
+    func processSpotifyData(_ completion: @escaping (_ success: Bool) -> Void)
+    func getTopArtistSongDataFromFirebase()
+}
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, SpotifyProtocol {
+        
     @IBOutlet weak var tableView: UITableView!
     let sharedCellIdentifier = "SharedCard"
     let imageCellIdentifier = "ImageCard"
+    public var spotify: Spotify? = nil
+    private var topArtistCancellables: AnyCancellable? = nil
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +36,34 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.register(UINib.init(nibName: "SharedCard", bundle: nil), forCellReuseIdentifier: sharedCellIdentifier)
         tableView.register(UINib.init(nibName: "ImageCard", bundle: nil), forCellReuseIdentifier: imageCellIdentifier)
         
+        spotify = Spotify()
+        print("Configure Spotify Authorization")
+        if (!(spotify!).isUserAuthSavedToKeychain()) {
+            print("Authorizing Spotify...")
+            self.performSegue(withIdentifier: "authorizeSpotify", sender: nil)
+        } else {
+            print("Spotify Authorized...")
+            processSpotifyData() { completion in
+                if completion {
+                    self.getTopArtistSongDataFromFirebase()
+                } else {
+                    print("Error processing Spotify data")
+                }
+                
+            }
+            
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "authorizeSpotify",
+           let connectSpotifyVC = segue.destination as? ConnectSpotifyViewController {
+            connectSpotifyVC.delegate = self
+            connectSpotifyVC.spotify = spotify
+        }
+    }
+    
+    func getTopArtistSongDataFromFirebase() {
         self.fetchUserSongArtistData { completion in
             if completion {
                 // Loading Screen should be false at this point
@@ -38,6 +74,93 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             } else {
                 print("error")
             }
+        }
+    }
+    
+    func processSpotifyData(_ completion: @escaping (_ success: Bool) -> Void) {
+        print("Spotify Authorized...")
+        // Get user's top artists
+        print("Retrieving user's top artists")
+        self.processTopArtists()
+        // Get user's top tracks
+        print("Retrieving user's top tracks")
+        self.processTopSongs()
+        
+        // Reload table view
+        // to reflect data about top artists
+        // and top tracks
+        completion(true)
+    }
+    
+    func processTopArtists() {
+        var topArtists = [Artist]()
+        // Pull user's top artists from Spotify
+        self.topArtistCancellables = spotify!.api
+            .currentUserTopArtists(.shortTerm)
+                    .receive(on: RunLoop.main)
+                    .sink(
+                        receiveCompletion: self.receiveTopArtistsCompletion(_:),
+                        receiveValue: { topArtistsResponse in
+                            topArtists = topArtistsResponse.items
+                            // Parse top artist data
+                            // Get artist name, genre?, image?
+                            var topArtistNames = [String]()
+                            for artist in topArtists {
+                                topArtistNames.append(artist.name)
+                            }
+                            // Load user's top artist data into Firebase
+                            // Add artist to user top artist
+                            self.loadTopArtistsToFirebase(topArtistNames: topArtistNames)
+                        }
+                    )
+    }
+    
+    func processTopSongs() {
+        var topTracks = [Track]()
+        // Pull user's top artists from Spotify
+        self.topArtistCancellables = spotify!.api
+            .currentUserTopTracks(.shortTerm)
+                    .receive(on: RunLoop.main)
+                    .sink(
+                        receiveCompletion: self.receiveTopArtistsCompletion(_:),
+                        receiveValue: { topTracksResponse in
+                            topTracks = topTracksResponse.items
+                            // Parse top artist data
+                            // Get artist name, genre?, image?
+                            var topSongs = [String:String]()
+                            for track in topTracks {
+                                topSongs[track.name] = track.artists?[0].name
+                            }
+                            // Load user's top artist data into Firebase
+                            // Add artist to user top artist
+                            self.loadTopSongsToFirebase(topSongs: topSongs)
+                        }
+                    )
+    }
+    
+    func loadTopSongsToFirebase(topSongs: [String: String]) {
+        let currentUser = Auth.auth().currentUser?.uid
+        let db = Firestore.firestore()
+        let ref = db.collection("Users")
+        let document = ref.document(currentUser!)
+        document.setData(["Top Songs": topSongs], merge: true)
+        print("Added user's top songs to Firebase: \(topSongs)")
+    }
+    
+    func loadTopArtistsToFirebase(topArtistNames: [String]) {
+        let currentUser = Auth.auth().currentUser?.uid
+        let db = Firestore.firestore()
+        let ref = db.collection("Users")
+        let document = ref.document(currentUser!)
+        document.setData(["Top Artists": topArtistNames], merge: true)
+        print("Added user's top artists to Firebase: \(topArtistNames)")
+    }
+    
+    func receiveTopArtistsCompletion(
+        _ completion: Subscribers.Completion<Error>
+    ) {
+        if case .failure(let error) = completion {
+            print("Couldn't retrieve top artists: \(error)")
         }
     }
     
